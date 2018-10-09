5c91584aa0c88b0e4045a89d6a5b8cdddc98bbda
Andor Saga <andor.saga@gmail.com>
2018-10-09 14:34:43 -0400

    \.

--- src/js/entity/actors/Starfish.js
+++ src/js/entity/actors/Starfish.js
@@ -11,7 +11,7 @@ import Launcher from '../components/Launcher.js';
 import SpriteRender from '../components/SpriteRender.js';
 
 import BoundingCircle from '../../collision/BoundingCircle.js';
-import CollisionType from '../../collision/CollisionType.js';
+import CType from '../../collision/CollisionType.js';
 
 import createRocketBullet from './RocketBullet.js';
 
@@ -44,10 +44,7 @@ export default function createStarFish() {
 
     rocketGun.addComponent(new Health(rocketGun, 40));
     rocketGun.addComponent(new Killable(rocketGun));
-    rocketGun.addComponent(new Collidable(rocketGun, {
-      type: CollisionType.ENEMY,
-      mask: CollisionType.PLAYER_BULLET
-    }));
+    rocketGun.addComponent(new Collidable(rocketGun, { type: CType.ENEMY, mask: CType.PLAYER_BULLET }));
 
     rocketLauncher.updateProxy = function() {
       this.direction.x = Math.cos((a));
@@ -60,10 +57,7 @@ export default function createStarFish() {
   e.addComponent(new Killable(e));
   e.addComponent(new Health(e, { amt: 500 }));
   e.addComponent(new HealthRender(e, { layer: 100 }));
-  e.addComponent(new Collidable(e, {
-    type: CollisionType.ENEMY,
-    mask: CollisionType.PLAYER_BULLET
-  }));
+  e.addComponent(new Collidable(e, { type: CType.ENEMY, mask: CType.PLAYER_BULLET }));
 
   return e;
 }
